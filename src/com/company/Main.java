b8fe6e28b624dc6fccc12674bdb48f11c05fc943
BuildTools <unconfigured@null.spigotmc.org>
2019-10-06 03:39:53 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+    private static void print(String string){
+        System.out.println(string);
+    }
+    public static void main(String[] args) {
+	for(int i = 0; i < 200; i++) {
+        print("Hit or miss, I guess they never miss huh");
+        print("Nya, colonel");
+        print("Yeehaw pardner");
+            }
+        }
+    }
+
